a124084ab0dd4eb4dc6f9eccf4de138c33cff7a8
Donald Winkelman <dmwink.tx@gmail.com>
2025-02-15 23:14:08 -0500

    Integrated pygame

--- gym.py
+++ gym.py
@@ -2,6 +2,7 @@ import gymnasium as gym
 import numpy as np
 import random
 import math
+import pygame
 
 class DotGym(gym.Env):
 
@@ -20,13 +21,14 @@ class DotGym(gym.Env):
             for char in line:
                 print(char,end="")
             print("\n")
+        return charArr
 
 
     def __init__(self,size):
         self.size = size
         self.stepNum = 0
-        self.target_pos = np.random.random_integers(0,self.size-1,size=2)
-        self.agent_pos = np.random.random_integers(0,self.size-1,size=2)
+        self.target_pos = np.random.randint(0,self.size-1,size=2)
+        self.agent_pos = np.random.randint(0,self.size-1,size=2)
         self.obs_space = gym.spaces.Dict({
             "agent":gym.spaces.Box(0,size-1,shape=(2,),dtype=int),
             "target":gym.spaces.Box(0,size-1,shape=(2,),dtype=int)
@@ -41,11 +43,11 @@ class DotGym(gym.Env):
 
     def reset(self,seed):
         super().reset(seed)
-        self.target_pos = np.random.random_integers(0,self.size-1,size=2)
-        self.agent_pos = np.random.random_integers(0,self.size-1,size=2)
+        self.target_pos = np.random.randint(0,self.size-1,size=2)
+        self.agent_pos = np.random.randint(0,self.size-1,size=2)
         self.stepNum = 0
         return {"agent":self.agent_pos,"target":self.target_pos}
-    
+
     def step(self,action):
         self.agent_pos = list(np.clip(np.array(self.agent_pos)+np.array(self.action_to_direction[action]),0,self.size-1))
         reward = -1*self.distance(self.target_pos,self.agent_pos)
@@ -55,9 +57,37 @@ class DotGym(gym.Env):
         self.display()
         return {"agent":self.agent_pos,"target":self.target_pos},reward,terminated,truncated
 
-mygym = DotGym(5)
-action = random.randint(0,3)
-while not mygym.step(action)[2]: #while it's not terminated
-    action = random.randint(0,3)
+pygame.init()
+screen = pygame.display.set_mode((320, 320))
+samuel = pygame.transform.scale(pygame.image.load("samuel.png").convert_alpha(), (64, 64))
+goose = pygame.transform.scale(pygame.image.load("goose.png").convert_alpha(), (64, 64))
+table = pygame.transform.scale(pygame.image.load("table.png").convert_alpha(), (64, 64))
 
-        
+mygym = DotGym(5)
+
+clock = pygame.time.Clock()
+running = True
+x = 0
+delta_time = 0.1
+
+action = random.randint(0, 3)
+while not mygym.step(action)[2]: # while it's not terminated
+    screen.fill((255, 255, 255))
+    display = mygym.display()
+    for x in range(5):
+        for y in range(5):
+            toBlit = None
+            emoji = display[x][y]
+            match emoji:
+                case '⬜️':
+                    toBlit = goose
+                case '🤮':
+                    toBlit = samuel
+                case '🎯':
+                    toBlit = table
+            screen.blit(toBlit, (64*x, 64*y))
+    action = random.randint(0,3)
+    pygame.display.flip()
+    delta_time = clock.tick(1) / 1000
+    delta_time = max(0.001, min(0.1, delta_time))
+
